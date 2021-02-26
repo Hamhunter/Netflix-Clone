b396421bef1592e30071aca6ed559375aecb5d2d
hamdan_abdul <hamdan@anvayaanalytics.com>
2021-02-26 16:14:07 +0530

    added prerequisites

---
+++ netflix-clone/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import { SliderComponent } from './components/slider/slider.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SliderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
